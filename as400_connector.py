83665e7c000ea58b07983ca09a9c0182f41a42f0
clark <egin400@gmail.com>
2024-10-04 13:42:30 +0800

    還原到還原點3

--- as400_connector.py
+++ as400_connector.py
@@ -1,37 +1,20 @@
 import jaydebeapi
-from queue import Queue
 
 jt400_path = "/Users/clark/Desktop/DDSC/Clark文件/13-JavaCode/jt400.jar"
 
-class AS400ConnectionPool:
-    def __init__(self, max_connections=5):
-        self.pool = Queue(maxsize=max_connections)
-        self.max_connections = max_connections
+class AS400Connector:
+    def __init__(self):
+        self.connections = {}
+        self.current_connection = None
 
-    def get_connection(self, host, user, password):
-        if not self.pool.empty():
-            return self.pool.get()
-        elif self.pool.qsize() < self.max_connections:
+    def connect_to_as400(self, host, user, password):
+        try:
             connection_string = f"jdbc:as400://{host}"
             connection = jaydebeapi.connect("com.ibm.as400.access.AS400JDBCDriver",
                                             connection_string,
                                             [user, password],
                                             jt400_path)
-            return connection
-        else:
-            raise Exception("連接池已滿")
-
-    def release_connection(self, connection):
-        self.pool.put(connection)
-
-class AS400Connector:
-    def __init__(self):
-        self.connection_pool = AS400ConnectionPool()
-        self.current_connection = None
-
-    def connect_to_as400(self, host, user, password):
-        try:
-            connection = self.connection_pool.get_connection(host, user, password)
+            self.connections[host] = connection
             self.current_connection = host
             return connection, None
         except Exception as e:
